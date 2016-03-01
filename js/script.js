c9a0f8abddd4a4507370c9008fdfa1201da77c13
Rustam Yusupov <rustam.yusupov@gmail.com>
2016-03-01 18:29:36 +0500

    Timer in work

--- js/script.js
+++ js/script.js
@@ -12,13 +12,10 @@ document.addEventListener('mozfullscreenchange', changeBtnImage);
 document.addEventListener("MSFullscreenChange", changeBtnImage);
 document.addEventListener('fullscreenchange', changeBtnImage);
 
-function startStopTimer() {
-  changeBtnCaption();
 
-}
 
 function resetTimer() {
-
+  clearInterval(timerId);
 }
 
 function lapTime() {
@@ -68,4 +65,47 @@ function toggleFullScreen() {
       document.webkitExitFullscreen();
     }
   }
+}
+
+
+
+var clockDigit = document.querySelector(".stopwatch-clock .clock-digit");
+var startTime;
+var timerId;
+
+function startStopTimer() {
+  // changeBtnCaption();
+
+  startTime = now();
+  timerId = setInterval(setTime, 1);
+}
+
+
+function setTime() {
+
+  clockDigit.innerHTML = formatTime( now() - startTime );
+}
+
+function now() {
+  return (new Date()).getTime();
+}
+
+function formatTime(time) {
+	var h = m = s = ms = 0;
+	var newTime = '';
+
+	h = Math.floor( time / (60 * 60 * 1000) );
+	time = time % (60 * 60 * 1000);
+	m = Math.floor( time / (60 * 1000) );
+	time = time % (60 * 1000);
+	s = Math.floor( time / 1000 );
+	ms = time % 1000;
+
+	newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2) + ':' + pad(ms, 3);
+	return newTime;
+}
+
+function pad(num, size) {
+	var s = "0000" + num;
+	return s.substr(s.length - size);
 }
